412bd467100f8cb925ddebb826b34a6be3916505
Payas Bhrigu <bhrigu.payas10@gmail.com>
2022-05-29 11:51:36 +0530

    Assignment 3.3 - Explained the creation of new keys in 'stack' object.

--- Week-3/assignment4.js
+++ Week-3/assignment4.js
@@ -16,7 +16,7 @@ const stack = createStack();
 stack.push(10);
 stack.push(5);
 stack.pop();// => 5
-console.log(stack.items);//! => undefined
+console.log(stack.items);    //! => undefined
 
 //! In the earlier code for createStack, the variable items was getting returned as a part of the object that
 //! also contained the push() and pop() functions, so items could directly be accessed by the instance of this returned object 
@@ -24,3 +24,11 @@ console.log(stack.items);//! => undefined
 //! But now, putting the variable items outside of the return statement, outside of the object getting returned,
 //! it will now only be accessible to the variables or functions that have access to items as part of the lexical scope.
 //! So now items won't be accessible outside of its scope i.e createStack() function call.
+
+stack.items = [10,100,1000];
+console.log(stack.items)// => [ 10, 100, 1000 ]    //! => This is successfully run because with this we create
+//! a new key in the stack object. This is not the same 'items' variable as the one present in the function createStack()
+
+//! For Example,Creating another key:
+stack.elements = [1,2,3,4,5]
+console.log(stack.elements) //! => [ 1, 2, 3, 4, 5 ]
