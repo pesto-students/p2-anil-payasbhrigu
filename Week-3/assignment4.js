fe53463679f00b22f6af579df4484ab6c380a8ee
Payas Bhrigu <bhrigu.payas10@gmail.com>
2022-05-29 11:14:46 +0530

    Week 3 - Assignment 3.4

---
+++ Week-3/assignment4.js
@@ -0,0 +1,26 @@
+function createStack() 
+{
+    let items = [];
+    return{
+        push(item) {
+            items.push(item);
+        },
+        pop() {
+            // console.log(items); //!output : [ 10, 5 ]
+            return items.pop();
+        }
+    };
+}
+
+const stack = createStack();
+stack.push(10);
+stack.push(5);
+stack.pop();// => 5
+console.log(stack.items);//! => undefined
+
+//! In the earlier code for createStack, the variable items was getting returned as a part of the object that
+//! also contained the push() and pop() functions, so items could directly be accessed by the instance of this returned object 
+//! created outside, through the createStack function call.
+//! But now, putting the variable items outside of the return statement, outside of the object getting returned,
+//! it will now only be accessible to the variables or functions that have access to items as part of the lexical scope.
+//! So now items won't be accessible outside of its scope i.e createStack() function call.
