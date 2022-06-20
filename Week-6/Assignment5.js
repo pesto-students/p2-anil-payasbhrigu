04838dba0b52c355232976bd37e80c49a17e3415
Payas Bhrigu <bhrigu.payas10@gmail.com>
2022-06-20 16:13:32 +0530

    Week 6 - Assignment 6.5 (Pair With Difference)

---
+++ Week-6/Assignment5.js
@@ -0,0 +1,15 @@
+//Pair with given difference
+function checkPair(arr,diff){
+    let map = new Map();
+    for(let i = 0;i < arr.length;i++)
+        map.set(arr[i],1);
+    console.log(map)
+    for(let i = 0;i < arr.length;i++){
+        if(map.has(arr[i] + diff))
+            return 1;
+    }
+    return 0;
+}
+console.log(checkPair([5, 10, 3, 2, 50, 80],78))    //!=> 1
+console.log(checkPair([-10, 20],30))    //! => 1
+console.log(checkPair([-20, 20],30))    //! => 0
