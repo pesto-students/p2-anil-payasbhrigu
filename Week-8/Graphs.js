50702e25f149f49e04449adf28c9dfd43171cf3c
Payas Bhrigu <bhrigu.payas10@gmail.com>
2022-07-03 14:16:07 +0530

    Refactoring Graph Operations and Assignment 8.4

--- Week-8/Graphs.js
+++ Week-8/Graphs.js
@@ -22,5 +22,20 @@ function Graph(){
         // }
         return this.adjacencyList
     }
+
+    this.createUndirectedGraph = (arr) => {
+        let vertices = new Set()
+        for(let i = 0;i<arr.length;i++){
+            for(let j=0;j<arr[i].length;j++){
+                vertices.add(arr[i][j])
+            }
+        }
+        for(let vertex of vertices){
+            this.addVertex(vertex)
+        }
+        for(let pair of arr){
+            this.addEdge(pair[0],pair[1])
+        }
+    }
 }
 module.exports = {Graph}
