1d9a3e4a62dda2fbe7708c27ba692e94086b8783
Payas Bhrigu <bhrigu.payas10@gmail.com>
2022-07-19 23:18:33 +0530

    Week 10 - Assignment (URL-Shortener)

---
+++ Week-10/url-shortener/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Items from './Components/Items';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Items></Items>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
