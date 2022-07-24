f8672ffa6615fd2b1b5d5ed6116c7c842b49c7dc
Payas Bhrigu <bhrigu.payas10@gmail.com>
2022-07-24 23:53:13 +0530

    Week 11 - Assignment 2 (Step-Counter)

---
+++ Week-11/step-counter/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import StepCounter from './Components/StepCounter';
+
+function App() {
+
+  return (
+    <div className="App">
+      <StepCounter/>
+    </div>
+  );
+
+}
+
+export default App;
---
+++ Week-11/step-counter/src/Reducers/countReducer.js
@@ -0,0 +1,12 @@
+const counter = (state = 0,action) => {
+    switch (action.type) {
+        case "INCREMENT":
+            return state + 1
+        case "RESET":
+            return 0
+        default:
+            return state
+    }
+}
+
+export default counter;
